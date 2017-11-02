081e60b82479356f05377af4efdc45260bf6e356
Kevin Chen <31024369+kcwork2016@users.noreply.github.com>
2017-11-02 01:21:20 -0500

    Build 5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './services/junk/index.css';
-//import App from './services/junk/App';
-import QuizContainer  from '../src/containers/QuizContainer';
-import registerServiceWorker from './services/junk/registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './services/junk/index.css';
+// //import App from './services/junk/App';
+// import QuizContainer  from '../src/containers/QuizContainer';
+// import registerServiceWorker from './services/junk/registerServiceWorker';
+//
+// ReactDOM.render(<QuizContainer />, document.getElementById('root'));
+// registerServiceWorker();
 
-ReactDOM.render(<QuizContainer />, document.getElementById('root'));
-registerServiceWorker();
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, hasHistory } from 'react-router';
+import configureStore from './store/configureSore'
+
+const store = configureStore();
+
+render(
+    <Provider store= { store } >
+        <div>
+            <Router history={ hasHistory } routes={ routes } />
+        </div>
+
+    </Provider>,
+    document.getElementById('root')
+);
